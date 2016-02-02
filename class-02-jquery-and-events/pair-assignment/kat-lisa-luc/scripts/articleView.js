e81f2605edcd1a0bbe4a2e4e8f411783d3dbf451
lwenke01 <lwenke@gmail.com>
2016-02-02 14:53:40 -0800

    added category filter to hide/show

--- class-02-jquery-and-events/pair-assignment/kat-lisa-luc/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/kat-lisa-luc/scripts/articleView.js
@@ -49,10 +49,23 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
+
+    $('#category-filter').on('change',function() {
+      if ($(this).val()) {
+        $('article').hide();
+        var catVal = $(this).val();
+        console.log(catVal);
+        $('article[data-category="' + catVal + '"]').fadeIn();
+      }
+      else {
+        $('article').show();
+      }
+      $('#author-filter').val('');
+    });
 
 };
 
@@ -82,3 +95,4 @@ articleView.setTeasers = function() {
 $();
 articleView.populateFilters();
 articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
